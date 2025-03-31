0d1cc5c9de8d647086b6514029132b714b4e7e1d
Simone Piermattei <piermatteisimone@gmail.com>
2025-03-31 19:45:31 +0200

    first commit

---
+++ src/main/java/it/aulab/aulabchronicles/AulabchroniclesApplication.java
@@ -0,0 +1,13 @@
+package it.aulab.aulabchronicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulabchroniclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulabchroniclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/aulab/aulabchronicles/AulabchroniclesApplicationTests.java
@@ -0,0 +1,13 @@
+package it.aulab.aulabchronicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulabchroniclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
