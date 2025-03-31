bd73fc4b8ba79f46051a850f83ebef70dd544674
Simone Piermattei <piermatteisimone@gmail.com>
2025-03-31 21:26:15 +0200

    Eseguito lo scaffolding

---
+++ .sql/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO user (username, email, password, created_at) VALUES
+('admin', 'admin@aulab.it', '25f9e794323b453885f5181f1b624d0b');
+
+
+
+INSERT INTO roles (name) VALUES 
+('ROLE_ADMIN'),
+('ROLE_REVISOR'),
+('ROLE_WRITER'),
+('ROLE_USER');
+
+
+INSERT INTO user_roles (user_id, role_id) values 
+(1,1);
+
+
+INSERT INTO categories (name) values 
+('politica'),
+('economia'),
+('food&drink'),
+('business'),
+('sport'),
+('intrattenimento'),
+('tech');
+
